fcde08f68468151a2ff40e163e01b38ee4554ef1
Christian Schuldt <christian@schuldt-it.de>
2016-10-28 01:54:04 +0200

    updated version of the initial code
    Summary: to review in phabricator
    
    Test Plan: none
    
    Reviewers: skemnitz
    
    Reviewed By: skemnitz
    
    Differential Revision: http://tesla4.physik.uni-greifswald.de:2000/D253

--- web_ui.cpp
+++ web_ui.cpp
@@ -6,15 +6,25 @@
 
 
 #include "web_ui.h"
+#include <map>
 #include <Wt/WBootstrapTheme>
 #include <Wt/WMenu>
 #include <Wt/WStackedWidget>
+#include <Wt/WMessageBox>
+#include <Wt/WNavigationBar>
+#include <Wt/WPopupMenu>
+#include <Wt/WPopupMenuItem>
 #include <Wt/WContainerWidget>
+#include <Wt/WAnchor>
 #include <Wt/WLineEdit>
+#include <Wt/WLink>
+#include <Wt/WText>
+#include <Wt/WTextArea>
 #include <Wt/WPushButton>
 #include <Wt/WTemplate>
 #include <Wt/WApplication>
 #include <Wt/WServer>
+#include <Wt/WHBoxLayout>
 #include <iostream>
 #include <sstream>
 
@@ -31,56 +41,133 @@ public:
   WebUIApplication(const WEnvironment& env);
 
 private:
-  WLineEdit *nameEdit_;
-};
 
+  //define class
+
+  WLineEdit *nameEdit_;
+  WTextArea *command_output;
+  std::map<string,string> key_to_command;	    // key value map command file key_to_command
+
+ void GetCommandoutput( std::string, WTextArea*);   // read command output line
+ void read_input_file(std::string filename);	    // 
+};
 
 WebUIApplication::WebUIApplication(const WEnvironment& env)
   : WApplication(env)
 {
   setTitle("poolui");                               // application title
+
+// css
   Wt::WBootstrapTheme *bootstrapTheme = new Wt::WBootstrapTheme(this);
   bootstrapTheme->setVersion(Wt::WBootstrapTheme::Version3);
   bootstrapTheme->setResponsive(true);
   setTheme(bootstrapTheme);
-  useStyleSheet("resources/themes/bootstrap/3/bootstrap-theme.min.css");
+  useStyleSheet("resources/themes/bootstrap/3/bootstrap-theme.min.css"); 
 
   // needed to load the templates for the objects and make the bootstrap theme work
   this->messageResourceBundle().use(this->appRoot() + "text");
-  
-  Wt::WContainerWidget *main_container = new Wt::WContainerWidget();
 
-  root()->addWidget( main_container );
+  // Config file read
+   read_input_file("key_to_command");
+  // menuContainer + menu
+  Wt::WContainerWidget *menuContainer = new Wt::WContainerWidget();
+    root()->addWidget(menuContainer);
 
-  nameEdit_ = new WLineEdit();
-  main_container->addWidget( nameEdit_ );
-
-  Wt::WPushButton* pb = new Wt::WPushButton("Execute Action");
-  main_container->addWidget( pb );
-
-  pb->clicked().connect( std::bind( [=] () {
-    FILE *in;
-    char buff[512];
+  Wt::WNavigationBar *navigation = new Wt::WNavigationBar(menuContainer);
+     navigation->setTitle("Link","http://www.google.com/search?q=corpy+inc");
+      navigation->setResponsive(true); 
       
-	if(!(in = popen("ls -sail", "r"))){
-        return 1;
-	  }
-	    while(fgets(buff, sizeof(buff), in)!=NULL){
-	        cout << buff;
-		  }
-		    pclose(in);
-		    return 0;
-	}
-  ));
+      // output WText menuItem
+     Wt::WStackedWidget *contentsStack = new Wt::WStackedWidget(menuContainer);
+      contentsStack->addStyleClass("contents");
+
+      // Setup a Left-aligned	  
+      Wt::WMenu *leftMenu = new Wt::WMenu(contentsStack, menuContainer);
+      navigation->addMenu(leftMenu);
+
+          leftMenu->addItem("Home",new Wt::WText("alternative Text"));
+	  leftMenu->addItem("link2", new Wt::WText("link2"));      
+          leftMenu->addItem("link3", new Wt::WText("link3"));
+ 
+  // contentContainer + content
+   Wt::WContainerWidget *contenContainer = new Wt::WContainerWidget();
+     root()->addWidget(contenContainer);
+
+    //button 
+	  Wt::WPushButton *pb1 = new Wt::WPushButton("Button1" );
+	  root()->addWidget(pb1);
+	  
+	  Wt::WPushButton *pb2 = new Wt::WPushButton("Button2" );
+	  root()->addWidget(pb2);
+
+
+          command_output = new WTextArea();
+          root()->addWidget( command_output );
+
+
+   //action button 
+   pb1->clicked().connect(std::bind([=](){
+	GetCommandoutput(key_to_command["date"] , command_output );
+       }
+    ));
+   pb2->clicked().connect(std::bind([=](){
+	GetCommandoutput( "ls -ltr", command_output );
+       }
+    ));
 
 
 }
 
-WApplication *createApplication(const WEnvironment& env)
+  // CommandLine to TextAarea
+void WebUIApplication::GetCommandoutput( std::string command, WTextArea* text_area){
+  std::string text; 
+  FILE *in;
+  char buff[512];
+   if(!(in = popen(command.c_str(), "r"))){
+       return ;
+     }
+     while(fgets(buff, sizeof(buff), in)!=NULL){
+ 
+         std::string line(&buff[0]);
+    text+= line;
+
+	 //	cout << buff ;  
+       }
+     pclose(in);
+      text_area->setText(text); 
+      return ;
+  }
+   
+
+  WApplication *createApplication(const WEnvironment& env)
 {
   return new WebUIApplication(env);
 }
 
+void WebUIApplication::read_input_file( std::string filename ) {
+  ifstream in(filename);
+  assert( in.good() );
+  
+  std::string line;
+  while( getline( in, line ) ){
+    stringstream line_stream( line );
+    std::string key, value_part;
+    std::string combined_value;
+    line_stream >> key ;
+    if (key[0] == '#' || key == ""){
+      continue;
+    }
+    while ( line_stream >> value_part ) {
+      combined_value += value_part + " ";
+    }
+    key_to_command[key] = combined_value;
+  }
+  for( auto&& element : key_to_command ){
+    cout << element.first << " " << element.second << endl;
+      
+  }
+  
+}
 
 int WebUI::run(int argc, char **argv)
 {
